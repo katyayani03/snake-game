dd410311b9769f9ec7556f581a98c192cb4c0ec4
katyayani03 <162354868+katyayani03@users.noreply.github.com>
2024-08-03 19:58:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ from  food import  Food
 from  scoreboard import Scoreboard
 import time
 
+#create the board
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snakesss")
 screen.tracer(0)
 
+#create the snake, food particle and the scoreboard
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+#move the snake using direction keys
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
+#have the snake moving continously
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.1) #Increase this value if you want to slow down yowr snake
     snake.move()
 
     # detect collission with food
